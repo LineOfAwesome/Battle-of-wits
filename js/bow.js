0985f08797ef857f02e18a6ffd469524cd767a69
Gervel Giva <gervelgiva@gmail.com>
2013-12-07 16:01:29 +0800

    pass option

--- js/bow.js
+++ js/bow.js
@@ -1,19 +1,39 @@
 PLAYERS = [];
 CURRENT_PLAYER = 0;
 
+ACTION = {
+	attack: {
+		target: "unit",
+		cost:{
+			type: "none",
+			value: "1~2"
+		}
+	},
+	block: {
+		target: "self",
+		cost:{
+			type: "none",
+			value: "0"
+		}
+	},
+	heal: {
+		target: "self"
+	}
+};
+
 
 $(function(){
 	$(".state").not("#state-menu").hide();
-	$(".btn-set").on("click", ".m-btn", btnHandler);
+	$(".btn-set").on("click", ".m-btn, .po-btn", btnHandler);
 	$("#player-list").on("click", ".player-list-item", viewPlayer);
 });
 
 /* Handlers */
 
-
 function changeState(state){
 	$(".state").not("#state-" + state).hide();
-	$(".state#state-" + state).show()
+	$(".state#state-" + state).show();
+	$("#player-list").hide();
 }
 
 function startGame(){
@@ -24,24 +44,38 @@ function startGame(){
 	});
 	PLAYERS = shuffle(PLAYERS);
 	CURRENT_PLAYER = 0;
-	log(PLAYERS[CURRENT_PLAYER].name + "'s turn");
 
-	var out = "";
+	setPlayerList();
+	viewPlayer();
+}
+
+function setPlayerList(){
+	var out = "<h2 class='text-center'>Select a Player</h2>";
 	for(i in PLAYERS){
-		out += "<a href='#' data-id='" + i + "' class='player-list-item'>" + PLAYERS[i].name + "</a><br />";
+		out += "<a href='#' data-id='" + i + "' class='player-list-item form-control btn btn-default bot-space'>" + PLAYERS[i].name + "</a><br />";
 	}
-	$("#player-list").html(out);
-
-	viewPlayer(CURRENT_PLAYER);
+	out += "<hr />";
+	$("#player-list").html(out).hide();
 }
 
 function viewPlayer(){
 	var index = $(this).data("id") || CURRENT_PLAYER;
 	var player = PLAYERS[index];
 
-	$("#player-info #p-name").text(player.name);
-	$("#player-info #p-hp").text(player.cur_hp + "/" + player.max_hp);
-	$("#player-info #p-mp").text(player.cur_mp + "/" + player.max_mp);
+	$("#cur-player").text(PLAYERS[CURRENT_PLAYER].name);
+	$("#player-info #current-player").text(player.name);
+	$("#player-info #p-hp").width((player.cur_hp/player.max_hp) * 100+ "%");
+	$("#player-info #p-mp").width((player.cur_mp/player.max_mp) * 100+ "%");
+}
+
+function addPlayerInput(){
+	var l = $(".player-name").length + 1;
+	if(l < 5){
+		var input = "<input type='text' class='form-control text-center bot-space player-name' placeholder='Player "+l+"'>";
+		$("#player-names").append(input);	
+	}else{
+		log("Too much dude..");
+	}
 }
 
 function btnHandler(){
@@ -55,20 +89,25 @@ function btnHandler(){
 			changeState("guide");
 			break;
 		case "add player":
-			var l = $(".player-name").length + 1;
-			if(l < 5){
-				var input = "<input type='text' class='form-control text-center bot-space player-name' placeholder='Player "+l+"'>";
-				$("#player-names").append(input);	
-			}else{
-				log("Too much dude..");
-			}
+			addPlayerInput();
 			break;
 		case "start":
 			changeState("in-game");
 			startGame();
 			break;
+		case "pass":
+			CURRENT_PLAYER = (CURRENT_PLAYER + 1) % PLAYERS.length;
+			viewPlayer();
+			changeState("next-player-guide");
+			break;
+		case "next":
+			changeState("in-game");
+			break;
 		case "back":
 			changeState("menu");
+			break;
+		case "attack":
+			$("#player-list").show();
 			break;
 		case "exit":
 			log("exit");
@@ -84,7 +123,12 @@ function log(value){
 		out += value;
 	}else{
 		out += "<h3>Object</h3>";
-		out += value;
+		if(value.constructor == Array){
+			out += value.toString();
+		}
+		else if(value.constructor == Object){
+			out += JSON.stringify(value);
+		}
 	}
 	$("#log").html(out);
 }
@@ -107,4 +151,8 @@ function shuffle(array) {
 
 var Player = function(name, hp, mp){
 	return {name: name, cur_hp: hp, max_hp: hp, cur_mp: mp, max_mp: mp};
+}
+
+function randomRange(min, max){
+	return parseInt( min + (Math.random() * ( (max+1) - min) ) );
 }
